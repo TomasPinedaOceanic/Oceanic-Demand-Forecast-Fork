fbc54c94cd23884476ac7233656bfdb52a3e4a88
Bosnape <pcabrejosm@gmail.com>
2026-03-09 01:24:40 -0500

    Add inventory reference dataset and create .py file for Sprint 2: Inventory Optimization

---
+++ inventory/inventory_analysis.py
@@ -0,0 +1,5 @@
+# TODO: Implement inventory analysis
+# 1. Read inventory_snapshot from the DB
+# 2. Read predictions from the DB (avg_daily_forecast per SKU)
+# 3. Calculate FR-09, FR-10, FR-11, FR-12
+# 4. Save results to inventory_analysis
